09ec7e6f104c819e3461241c95bc259839c12712
Pedro Vicari <peehvicari@gmail.com>
2021-04-23 19:40:59 +0000

    Initial configs

---
+++ docker-compose/mysql/init_db.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS `teste`;
+
+CREATE TABLE `teste` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+INSERT INTO `teste` (name) VALUES ('Teste1'),('Teste2');
